eea2a08d56edd535d2f173192b987c61390e7d8d
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-20 00:48:57 +0530

    f

---
+++ src/app/addfasion/addfasion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addfasion',
+  templateUrl: './addfasion.component.html',
+  styleUrls: ['./addfasion.component.css']
+})
+export class AddfasionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddfasionComponent } from './addfasion/addfasion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddfasionComponent
   ],
   imports: [
     BrowserModule,
