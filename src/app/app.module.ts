21be03560c2d285e0965b2cc4cfa983d987c8e04
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-20 01:04:06 +0530

    f

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddfasionComponent } from './addfasion/addfasion.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ViewallfasionComponent } from './viewallfasion/viewallfasion.component';
 
+const link:Routes=[
+  {
+    path:"",component:AddfasionComponent
+  },
+  {
+    path:"view",component:ViewallfasionComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
     AddfasionComponent,
-    NavbarComponent
+    NavbarComponent,
+    ViewallfasionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(link)
   ],
   providers: [],
   bootstrap: [AppComponent]
